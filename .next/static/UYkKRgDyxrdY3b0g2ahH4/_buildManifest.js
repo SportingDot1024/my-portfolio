d60142dd3814ffc36897515ea73e3281ec38118a
SportDot1024 <shriram_kannuri@outlook.com>
2024-07-24 18:10:34 +0530

    changes

---
+++ .next/static/UYkKRgDyxrdY3b0g2ahH4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/624-fbe608f9b3bbed83.js","static/chunks/pages/index-f9c64dbce10eda1a.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
